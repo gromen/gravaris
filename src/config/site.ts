55e34242ade514c003d6ad629a16f019e900670b
Marcin Gromek <gromson101@gmail.com>
2026-02-05 13:16:23 +0100

    feat: Dodano informacje o autorze do stopki i konfiguracji witryny, oraz poprawiono semantykę elementów wiersza funkcji.

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,4 +7,8 @@ export const SITE_CONFIG = {
   },
   services: ["Wykopy stawów", "Odmulanie", "Roboty ziemne"],
   additionalServices: ["Umacnianie skarp", "Projekty rekreacyjne"],
+  author: {
+    name: "Marcin Gromek",
+    linkedin: "https://www.linkedin.com/in/gromekmarcin/",
+  },
 };
